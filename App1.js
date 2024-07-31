79c82bbeef662472ff29a8a416ad9f44bb781794
Seth <96531908+elpoderoso1@users.noreply.github.com>
2024-07-31 09:34:12 -0300

    10

---
+++ App1.js
@@ -0,0 +1,22 @@
+let a=1, b=4, c=5, d=6, e=7, f=77
+let suma
+
+suma = a+b
+console.log(suma)
+
+suma = c+d
+console.log(suma)
+
+suma = e+f
+console.log(suma)
+
+function SumarN(n1, n2){
+    let total = n1 + n2
+    console.log(total)
+    return total
+}
+
+SumarN(a, b)
+SumarN(c, d)
+SumarN(e, f)
+SumarN(666, 777)
